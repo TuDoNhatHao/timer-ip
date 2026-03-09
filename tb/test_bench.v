47007c2e01722c34978fa508af4ee741bbd2eb6b
TuDoNhatHao <nhathao231203@gmail.com>
2026-03-09 15:53:26 +0700

    test_bench.v

--- tb/test_bench.v
+++ tb/test_bench.v
@@ -217,5 +217,284 @@ module test_bench;
             rd_en(TDR1, 2);
             counting_mode_checker;
         end
+        $display("Case: Counter after overflow");
+        $display("Case: Default mode");
+        default_mode(-16,15,exp_cnt);
+        dbg_mode = 0;
+        @(posedge clk);
+        exp_cnt++;
+        dbg_mode = 1;
+        rd_en(TDR0,2);
+        rd_en(TDR1,2);
+        counting_mode_checker;
         
+        $display("Case: Control mode");
+        for (integer i=0; i<9; i++) begin
+            div_val = i;
+            control_mode(-6,5,div_val,exp_cnt);
+            dbg_mode = 0;
+            repeat (1 << div_val) begin
+                @(posedge clk);
+            end
+            exp_cnt++;
+            dbg_mode = 1;
+            rd_en(TDR0,2);
+            rd_en(TDR1,2);
+            counting_mode_checker;
+        end
+        $display("Case: Halt mode");
+        $display("Case: Halt mode is acknowledged");
+        dbg_mode = 1;
+        wr_en(THCSR,32'h00000001);
+        rd_en(THCSR,3);
         
+        $display("Case: Counter's value when halted");
+        $display("Case: Default mode");
+        default_mode(500,20,exp_cnt);
+        dbg_mode = 1;
+        wr_en(THCSR,32'h00000001);
+        rd_en(TDRO,2);
+        rd_en(TDR1,2);
+        counting_mode_checker;
+        
+        $display("Case: Control mode");
+        control_mode(5,5,4'b0010,exp_cnt);
+        dbg_mode = 1;
+        wr_en(THCSR,32'h00000001);
+        rd_en(TDRO,2);
+        rd_en(TDR1,2);
+        counting_mode_checker;
+        
+        $display("Case: After halted");
+        $display("Case: Halt_ack's value after halted");
+        dbg_mode = 1;
+        wr_en(THCSR,32'h00000001);
+        dbg_mode = 0;
+        wr_en(THCSR,32'h00000000);
+        if (tim_prdata === 32'h00000000) begin
+            $display("------------------------------------------------------------");
+            $display("%stime = %t ----------- PASS: Halt_ack signal is same as expected value -----------%s", GREEN, $time, RESET);
+            $display("------------------------------------------------------------");
+        end else begin
+            $display("------------------------------------------------------------");
+            $display("%stime = %t ----------- FAIL: Halt_ack signal is not same as expected value -----------%s", RED, $time, RESET);
+            $display("Exp:32'h00000003     Actual:%8h", tim_prdata);
+        end
+
+        $display("Case: Counter's value after halted");
+        $display("Case: Default mode");
+        default_mode(0,15,exp_cnt);
+        dbg_mode = 0;
+        @(posedge clk);
+        exp_cnt++;
+        dbg_mode = 1;
+        rd_en(TDR0,2);
+        rd_en(TDR1,2);
+        counting_mode_checker;
+        
+        $display("Case: Control mode");
+        for (integer i=0; i<9; i++) begin
+            div_val = i;
+            control_mode(0,6,div_val,exp_cnt);
+            dbg_mode = 0;
+            repeat (1 << div_val) begin
+                @(posedge clk);
+            end
+            exp_cnt++;
+            dbg_mode = 1;
+            rd_en(TDR0,2);
+            rd_en(TDR1,2);
+            counting_mode_checker;
+        end
+
+        $display("Case: Interrupt");
+        $display("Case: Interrupt pending bit");
+        wr_en(TISR,32'h00000000);
+        wr_en(TCR,32'h00000001);
+        setting_int_st;
+        rd_en(TISR,4);
+        
+        $display("Case: Timer interrupt");
+        wr_en(TISR,32'h00000000);
+        wr_en(TIER,32'h00000001);
+        setting_int_st;
+        if (tim_int == 1) begin
+            $display("------------------------------------------------------------");
+            $display("%stime = %t----------------PASS: Timer interrupt is asserted----------------%s", GREEN, $time, RESET);
+            $display("------------------------------------------------------------");
+        end else begin
+            $display("------------------------------------------------------------");
+            $display("%stime = %t----------------FAIL: Timer interrupt is not asserted----------------%s", RED, $time, RESET);
+            $display("Exp:1        Actual:%1b", tim_int);
+        end
+        
+        $display("Case: Reset timer interrupt");
+        wr_en(TISR,32'h00000000);
+        wr_en(TIER,32'h00000001);
+        setting_int_st;
+        wr_en(TISR,32'h00000001);
+        if (tim_int == 1) begin
+            $display("------------------------------------------------------------");
+            $display("%stime = %t-----PASS: Timer interrupt is not asserted when interrupt pending bit is cleared-----%s", GREEN, $time, RESET);
+            $display("------------------------------------------------------------");
+        end else begin
+            $display("------------------------------------------------------------");
+            $display("%stime = %t-----FAIL: Timer interrupt is asserted when interrupt pending bit is cleared-----%s", RED, $time, RESET);
+            $display("Exp:1        Actual:%1b", tim_int);
+        end
+
+        $display("Case: APB slave / Register");
+        $display("Case: Byte access");
+        for (integer i = 0; i<16; i++) begin
+            tim_pstrb = i;
+            $display("=======================================");
+            $display("%s===========Case: pstrb = 4'b%4b===========%s", CYAN, tim_pstrb, RESET);
+            $display("=======================================");
+            for (integer j = 0; j<=28; j = j+4) begin
+                err = 0;
+                tim_paddr = j;
+                register_name(tim_paddr);
+                byte_access_test(tim_pstrb, tim_paddr, 32'h00000000);
+                byte_access_test(tim_pstrb, tim_paddr, 32'h55555557);
+                byte_access_test(tim_pstrb, tim_paddr, 32'haaaaaaaa);
+                byte_access_test(tim_pstrb, tim_paddr, 32'hffffffff);
+                if (err === 0) begin
+                    $display("------------------------------------------------------------");
+                    $display("%stime = %t================PASS: Byte access is successful================%s", GREEN, $time, RESET);
+                    $display("------------------------------------------------------------");
+                end
+            end
+        end
+
+        $display("Case: Wait state");
+        rd_en(tim_paddr,6);
+        
+        $display("Case: Error handling");
+        $display("Case: Error occurs when select prohibited div_val");
+        wr_en(TCR,32'h00000000);
+        wr_en_err(TCR,32'h00000f00);
+        
+        $display("Case: Error occurs when div_en changes while timer_en is HIGH");
+        wr_en(TCR,32'h00000001);
+        wr_en_err(TCR,32'h00000003);
+        
+        $display("Case: Error occurs when div_val changes while timer_en is HIGH");
+        wr_en(TCR,32'h00000000);
+        wr_en(TCR,32'h00000003);
+        wr_en_err(TCR,32'h00000503);
+        
+        //tim_pstrb[0] = 0 coverage error signal
+        tim_pstrb = 4'b1111;
+        wr_en(TCR,32'h00000001);
+        tim_pstrb = 4'b0010;
+        wr_en(TCR,32'h00000030);
+        tim_pstrb = 4'b1111;
+
+        $display("Case: Internal counter when counting speed is not divided");
+        dbg_mode = 0;
+        wr_en(TCR,32'h00000000);
+        wr_en(TCR,32'h00000000);
+        rd_en(TDR0,2);
+        rd_en(TDR1,2);
+        wr_en(TCR,32'h00000003);
+        timer_en = wdata[0];
+        div_en = wdata[1];
+        speed = 4'b0000;
+        internal_counter_exp(speed,exp_int_cnt);
+        dbg_mode = 1;
+        rd_en(TDR0,2);
+        rd_en(TDR1,2);
+        internal_counter_checker;
+        
+        $display("Case: Internal counter when control mode is disabled");
+        dbg_mode = 0;
+        wr_en(TCR,32'h00000000);
+        wr_en(TCR,32'h00000000);
+        rd_en(TDR0,2);
+        rd_en(TDR1,2);
+        wr_en(TCR,32'h00000201);
+        timer_en = wdata[0];
+        div_en = wdata[1];
+        speed = wdata[11:8];
+        internal_counter_exp(speed,exp_int_cnt);
+        dbg_mode = 1;
+        rd_en(TDR0,2);
+        rd_en(TDR1,2);
+        dbg_mode = 0;
+        internal_counter_checker;
+
+        $display("Case: Internal counter when timer is disabled");
+        dbg_mode = 0;
+        wr_en(TCR,32'h00000000);
+        wr_en(TCR,32'h00000000);
+        rd_en(TDR0,2);
+        rd_en(TDR1,2);
+        wr_en(TCR,32'h00000202);
+        timer_en = wdata[0];
+        div_en = wdata[1];
+        speed = wdata[11:8];
+        internal_counter_exp(speed,exp_int_cnt);
+        dbg_mode = 1;
+        rd_en(TDR0,2);
+        rd_en(TDR1,2);
+        dbg_mode = 0;
+        internal_counter_checker;
+        #100;
+        $finish;
+    end
+
+    task register_name;
+        input [11:0] addr;
+        begin
+            case (addr)
+                12'h00: $display("%s==================== Register TCR ====================%s", CYAN, RESET);
+                12'h04: $display("%s==================== Register TDRO ====================%s", CYAN, RESET);
+                12'h08: $display("%s==================== Register TDR1 ====================%s", CYAN, RESET);
+                12'h0c: $display("%s==================== Register TCMP0 ====================%s", CYAN, RESET);
+                12'h10: $display("%s==================== Register TCMP1 ====================%s", CYAN, RESET);
+                12'h14: $display("%s==================== Register TIER ====================%s", CYAN, RESET);
+                12'h18: $display("%s==================== Register TISR ====================%s", CYAN, RESET);
+                12'h1c: $display("%s==================== Register THCSR ====================%s", CYAN, RESET);
+            endcase
+        end
+    endtask
+    
+    task test_case;
+        input [3:0] testcase;
+        begin
+            case (testcase)
+                0: rw_checker;
+                1: rw_checker_continuous(exp_data);
+                2: store_count_value(read_cnt);
+                3: halt_ack_checker;
+                4: int_status_checker;
+                5: byte_access_checker;
+                6: wait_state;
+            endcase
+        end
+    endtask
+
+    task byte_access;
+        input [3:0] tim_pstrb;
+        input [31:0] tim_pwdata;
+        begin
+            case (tim_pstrb)
+                4'b0001: wdata = {24'b0, tim_pwdata[7:0]};
+                4'b0010: wdata = {16'b0, tim_pwdata[15:8], 8'b0};
+                4'b0011: wdata = {16'b0, tim_pwdata[15:0]};
+                4'b0100: wdata = {8'b0, tim_pwdata[23:16], 16'b0};
+                4'b0101: wdata = {8'b0, tim_pwdata[23:16], 8'b0, tim_pwdata[7:0]};
+                4'b0110: wdata = {8'b0, tim_pwdata[23:8]};
+                4'b0111: wdata = {8'b0, tim_pwdata[23:0]};
+                4'b1000: wdata = {tim_pwdata[31:24], 24'b0};
+                4'b1001: wdata = {tim_pwdata[31:24], 16'b0, tim_pwdata[7:0]};
+                4'b1010: wdata = {tim_pwdata[31:24], 8'b0, tim_pwdata[15:8], 8'b0};
+                4'b1011: wdata = {tim_pwdata[31:24], tim_pwdata[15:0]};
+                4'b1100: wdata = {tim_pwdata[31:16], 16'b0};
+                4'b1101: wdata = {tim_pwdata[31:16], 8'b0, tim_pwdata[7:0]};
+                4'b1110: wdata = {tim_pwdata[31:8], 8'b0};
+                4'b1111: wdata = tim_pwdata[31:0];
+                default: wdata = 32'b0;
+            endcase
+        end
+    endtask
